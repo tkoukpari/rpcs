0c65cd6c0ffd208085a279333497e55d36f17703
Tiam Koukpari <tiamkoukpari@Tiams-MacBook-Air.local>
2025-07-06 16:59:10 -0400

    .

---
+++ server-client/rpc.ml
@@ -0,0 +1,10 @@
+open! Core
+open! Async
+
+module Query = struct
+  type t = unit
+end
+
+module Response = struct
+  type t = unit
+end
---
+++ server-client/rpc.mli
@@ -0,0 +1,10 @@
+open! Core
+open! Async
+
+module Query : sig
+  type t = unit
+end
+
+module Response : sig
+  type t = unit
+end
