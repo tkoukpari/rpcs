dc188fc71b7eb7b00ba75b9d820ed54998ab1287
Tiam Koukpari <tiamkoukpari@Tiams-MacBook-Air.local>
2025-07-09 23:52:19 -0400

    .

--- server-client/src/server.ml
+++ server-client/src/server.ml
@@ -1,11 +1,24 @@
 open! Core
 open! Async
 
-let param = Command.Param.(anon ("number" %: int))
+module State = struct
+  type t = { magic_number : int option } [@@deriving sexp_of]
+end
+
+let print_state_forever state ~how_often_should_i_print_state =
+  Clock.every how_often_should_i_print_state (fun () ->
+      Core.print_s [%message (state : State.t)]);
+  Deferred.never ()
+
+let param =
+  Command.Param.(anon ("how-often-should-i-print-state-in-seconds" %: float))
 
 let command =
-  Command.async ~summary:"Print an integer"
-    ~readme:(fun () -> "Accepts an integer and prints it")
-    (Command.Param.map param ~f:(fun n () ->
-         printf "Command received: %d\n" n;
-         return ()))
+  Command.async ~summary:""
+    (let%map.Command how_often_should_i_print_state = param in
+     fun () ->
+       let state : State.t = { magic_number = None } in
+       let how_often_should_i_print_state =
+         Time_float.Span.of_sec how_often_should_i_print_state
+       in
+       print_state_forever state ~how_often_should_i_print_state)
