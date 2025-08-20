a5f8f2230a79b8b1988c4b6eb47f471361359c11
Tiam Koukpari <tiamkoukpari@Tiams-MacBook-Air.local>
2025-08-20 17:35:25 -0400

    .

--- rpc/client/client.ml
+++ rpc/client/client.ml
@@ -6,8 +6,7 @@ let query_server ~inet_addr ~magic_number =
   let query : Protocol.Query.Latest.t = { magic_number } in
   let%bind connection =
     Rpc.Connection.client
-      (Tcp.Where_to_connect.of_inet_address
-         (`Inet (inet_addr, 8080)))
+      (Tcp.Where_to_connect.of_inet_address (`Inet (inet_addr, 8080)))
     >>| Result.ok_exn
   in
   let%bind connection_with_menu =
